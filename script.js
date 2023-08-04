8a0242f1af64007f43db2debea897b633098204b
Phyllali <90408077+phyllali@users.noreply.github.com>
2023-08-04 20:15:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/test.txt", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var content = xhr.responseText;
-    console.log(content);
-  }
-};
-xhr.send();
+function getContent(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var content = xhr.responseText;
+      return content;
+    }
+  };
+  xhr.send();
+}
+
+console.log(getContent("/test.txt"));
