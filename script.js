965d0987125ada2cb93536b5aeed4f9d7eb6e206
Phyllali <90408077+phyllali@users.noreply.github.com>
2023-08-04 21:09:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,18 +15,40 @@ function getContent(file) {
   });
 }
 
+function markdownToHtml(markdown) {
+  // Heading
+  markdown = markdown.replace(/(#+)(.*)/g, function(match, hashes, title) {
+    const level = hashes.length;
+    return `<h${level}>${title.trim()}</h${level}>`;
+  });
+
+  // Bold
+  markdown = markdown.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+
+  // Italic
+  markdown = markdown.replace(/\*(.*?)\*/g, '<em>$1</em>');
+
+  // Unordered List
+  markdown = markdown.replace(/^- (.*)/gm, '<ul><li>$1</li></ul>');
+
+  // Ordered List
+  markdown = markdown.replace(/^[0-9]+\. (.*)/gm, '<ol><li>$1</li></ol>');
+
+  // Line Break
+  markdown = markdown.replace(/  \n/g, '<br>');
+
+  // Paragraph
+  const paragraphs = markdown.split(/\n{2,}/g);
+  markdown = paragraphs.map((p) => `<p>${p}</p>`).join('');
+
+  return markdown;
+}
+
+var jsonObject;
 try {
   getContent("/wwwroot/index.json")
   .then(function (content) {
     const jsonObject = JSON.parse(content.toString());
-
-    for (const key in jsonObject) {
-      if (jsonObject.hasOwnProperty(key)) {
-        const obj = jsonObject[key];
-        console.log(`Object: ${key}`);
-        console.log(JSON.stringify(obj, null, 2));
-      }
-    }
     console.log(content);
   })
   .catch(function (error) {
@@ -34,4 +56,12 @@ try {
   });
 } catch (error) {
   console.error('Error parsing JSON:', error);
+}
+
+for (const key in jsonObject) {
+  if (jsonObject.hasOwnProperty(key)) {
+    const obj = jsonObject[key];
+    console.log(`Object: ${key}`);
+    console.log(JSON.stringify(obj, null, 2));
+  }
 }
