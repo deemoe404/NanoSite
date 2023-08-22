9746ff0de77b24c89b19051dbcde18255489ba5d
Phyllali <90408077+phyllali@users.noreply.github.com>
2023-08-22 11:09:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ function markdownToHtml(markdown) {
       .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
       .replace(/\*(.*?)\*/g, '<em>$1</em>')
       .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>')
-      .replace(/\`(.*?)\`/g, '<code>$1</code>');
+      .replace(/\`(.*?)\`/g, '<code>$1</code>')
+      .replace(/\~\~(.*?)\~\~/g, '<del>$1</del>');
 
     if (line.startsWith('#')) {
       const headingLevel = line.match(/^#+/)[0].length;
